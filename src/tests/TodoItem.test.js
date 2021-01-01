d51b44efb23dba41cb1af2933ed9a88ac5c05051
cocoder16 <cocoder16@gmail.com>
2021-01-01 17:56:52 +0900

    Update TodoItem & Remain question in concept-summary

--- src/tests/TodoItem.test.js
+++ src/tests/TodoItem.test.js
@@ -9,7 +9,6 @@ describe("<TodoItem />", () => {
     text: "TDD 배우기",
     done: false,
   };
-
   const setup = (props = {}) => {
     const initialProps = { todo: sampleTodo };
     const utils = render(<TodoItem {...initialProps} {...props} />);
@@ -28,5 +27,29 @@ describe("<TodoItem />", () => {
     const { span, button } = setup();
     expect(span).toBeTruthy();
     expect(button).toBeTruthy();
+  });
+
+  it("shows line-through on span when done is true", () => {
+    const { span } = setup({ todo: { ...sampleTodo, done: true } });
+    expect(span).toHaveStyle("text-decoration: line-through;");
+  });
+
+  it("does not show line-through on span when done is false", () => {
+    const { span } = setup({ todo: { ...sampleTodo, done: false } });
+    expect(span).not.toHaveStyle("text-decoration: line-through;");
+  });
+
+  it("calls onToggle", () => {
+    const onToggle = jest.fn();
+    const { span } = setup({ onToggle });
+    fireEvent.click(span);
+    expect(onToggle).toBeCalledWith(sampleTodo.id);
+  });
+
+  it("calls onRemove", () => {
+    const onRemove = jest.fn();
+    const { button } = setup({ onRemove });
+    fireEvent.click(button);
+    expect(onRemove).toBeCalledWith(sampleTodo.id);
   });
 });
