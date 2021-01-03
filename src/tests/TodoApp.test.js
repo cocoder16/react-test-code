30d63c89a64c7442b2f22209c60deb6f772791ea
cocoder16 <cocoder16@gmail.com>
2021-01-03 15:30:43 +0900

    Add create Todo logic

--- src/tests/TodoApp.test.js
+++ src/tests/TodoApp.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 
 import TodoApp from "components/TodoApp";
 
@@ -14,5 +14,18 @@ describe("<TodoApp />", () => {
     const { getByText } = render(<TodoApp />);
     getByText("TDD 배우기");
     getByText("react-testing-library 사용하기");
+  });
+
+  it("creates new todo", () => {
+    const { getByPlaceholderText, getByText } = render(<TodoApp />);
+    // 이벤트를 발생시켜서 새 항목을 추가하면
+    fireEvent.change(getByPlaceholderText("할 일을 입력하세요"), {
+      target: {
+        value: "새 항목 추가하기",
+      },
+    });
+    fireEvent.click(getByText("등록"));
+    // 해당 항목이 보여져야합니다.
+    getByText("새 항목 추가하기");
   });
 });
