00c82b25da164c4cde7c89186f4fee47634ec04c
cocoder16 <cocoder16@gmail.com>
2021-01-03 21:36:57 +0900

    Add axios sample component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import TodoApp from 'components/TodoApp';
 import DelayedToggle from 'components/DelayedToggle';
+import UserProfile from 'components/UserProfile';
 
 const App = () => {
   return (
     <>
       <TodoApp />
       <DelayedToggle />
+      <UserProfile id={1} />
     </>
   );
 };
