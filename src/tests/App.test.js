9c861e9505330be743c9aff6d6d046eaee1681b2
cocoder16 <cocoder16@gmail.com>
2021-01-17 16:42:54 +0900

    [RED] Counter integration test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,38 @@
 import React from 'react';
-import Counter from 'components/Counter';
+import App from 'App';
 import { render, fireEvent } from '@testing-library/react';
 
 // Counter 통합테스트
+
+describe('intergration test', () => {
+  const setup = (props = {}) => {
+    const utils = render(<App {...props} />);
+    const { getByTestId } = utils;
+    const resultText = getByTestId('result');
+    const plusBtn = getByText('+');
+    const minusBtn = getByText('-');
+    return {
+      ...utils,
+      counterComponent,
+      resultText,
+      plusBtn,
+      minusBtn,
+    };
+  };
+
+  describe('Counter', () => {
+    const { getByTestId, resultText, plusBtn, minusBtn } = setup();
+    getByTestId('Counter');
+
+    it('initial result is 0 rendered => click plus btn 2 times, minus btn 1 time => result 1 rendered', () => {
+      expect(resultText).toHaveTextContent(0);
+
+      for (let i = 0; i < 2; i++) {
+        fireEvent.click(plusBtn);
+        expect(resultText).toHaveTextContent(i + 1);
+      }
+      fireEvent.click(minusBtn);
+      expect(resultText).toHaveTextContent(1);
+    });
+  });
+});
